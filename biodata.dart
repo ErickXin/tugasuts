deaf66e755c5e703cdef3ce2c555efd4118c8be1
ErickXin <154953253+ErickXin@users.noreply.github.com>
2024-01-06 12:19:48 +0700

    Add files via upload

---
+++ biodata.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Biodata
+  String nama = "erick barsyah illahi";
+  int umur = 21;
+  double tinggi = 175.5;
+  String alamat = "Jl. padurenan km.1 desa cikakak";
+  String pekerjaan = "Mahasiswa";
+
+  // Menampilkan biodata
+  print("Biodata:");
+  print("Nama: $nama");
+  print("Umur: $umur tahun");
+  print("Tinggi: $tinggi cm");
+  print("Alamat: $alamat");
+  print("Pekerjaan: $pekerjaan");
+}
